90d603b1a58cb06ae7bf4b7ec7c22e310e1057bf
mo-alsafi <learning.ivi9@gmail.com>
2023-08-10 09:03:19 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is piece of art,\n")
+return (0);
+}
+
