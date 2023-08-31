5c56925b77e3423b3435a42bfa5f6e04c8b4ea30
mo-alsafi <learning.ivi9@gmail.com>
2023-08-31 20:44:53 +0300

    edit task 0 and adding mandatory tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,48 +1,20 @@
 #include "main.h"
 
 /**
- * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
- * Return: 0
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
 
-int wildcmp(char *s1, char *s2)
+void _puts_recursion(char *s)
 {
-	if (*s1 == '\0')
+	if (*s != '\0')
 	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 == '\0');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
-	if (*s2 == '*')
+	else
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	_putchar('\n');
 	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
-
-B
-B
-B
-B
-B
-B
-B
-B
-A
-A
-A
-A
-A
-A
-A
-A
-A
 }
